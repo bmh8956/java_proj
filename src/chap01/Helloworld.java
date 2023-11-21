586ba8f4106c874429945d1cd786377741bd5f6f
minhyeok Bang <bmh89895656@gmail.com>
2023-11-21 15:57:09 +0900

    231121 첫번째 커밋

---
+++ src/chap01/Helloworld.java
@@ -0,0 +1,16 @@
+package chap01;
+
+public class Helloworld {	//	class
+
+	public static void main(String[] args) {	//	main method
+		// 	주석 : 프로그램에서 처리되지 않는 블락
+		// 	한줄
+		/*
+			여러줄
+		 */ 
+		System.out.println("Hello World");
+		//	객체명 (클래스명).메소드
+
+	}
+
+}
